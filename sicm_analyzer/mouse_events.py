777ff1494a6dcc64defe4353cbe56a71ecd0ebd4
Heiko Leßlich <h.lesslich@gmx.de>
2022-10-03 20:26:14 +0200

    Refactored graph_canvas.py - added documentation - simplified mouse event methods for drawing rectangles Removed all unused code in mouse_events.py Added some explaining documentation to mouse_events.py Added some explaining documentation to line_profile_window.py Refactored draw line profile methods in main.py Added error handling for some methods in main.py either by catching exceptions or with if clauses Graph export now adds file extensions when none exists Removed unsupported file extensions from the filter

--- sicm_analyzer/mouse_events.py
+++ sicm_analyzer/mouse_events.py
@@ -1,10 +1,26 @@
-from datetime import time
-
 from PyQt6.QtCore import QPoint
+import numpy
+
+ROW = "row"
+COLUMN = "column"
 
 
 class MouseInteraction:
+    """A class for storing references to mouse pointer
+    coordinates.
 
+    Three mouse events can be bound:
+        - button_press_event: triggers when a mouse
+        button is pressed (the release is not necessary)
+        - motion_notify_event: triggers when moving
+        the mouse pointer
+        - button_release_event: triggers when a mouse
+        button is released
+
+    In args and kwargs optional arguments can be stored
+    which can, for example, be used to define conditions
+    when a mouse event should be registered.
+    """
     def __init__(self, *args, **kwargs):
         self.cid_move = None
         self.cid_press = None
@@ -15,86 +31,28 @@ class MouseInteraction:
         self.args = args
         self.kwargs = kwargs
 
-    def set_point_1_on_click(self, event):
-        if event.name == "button_press_event":
-            self.mouse_point1 = QPoint(int(event.xdata), int(event.ydata))
-            print(self.mouse_point1)
 
-
-def click_on_raster_image(event):
-    """A test function for getting the correct pixel after clicking on it.
-    TODO Clean up and move to another class
+# Helper functions
+# ______________________________________________________________________________________________________________________
+def is_in_range(point: QPoint, array: numpy.ndarray) -> bool:
     """
-    from sicm_analyzer.gui_main import SecondaryWindow
-    axes = event.canvas.figure.get_axes()[0]
-    print(event)
-    if event.inaxes == axes:
-        print("oben")
+    Checks if point coordinates are within the array size.
+
+    Note: ndarray.shape[0] is y and [1] is x!
+    """
+    return 0 <= point.x() <= array.shape[1] - 1 and 0 <= point.y() <= array.shape[0] - 1
+
+
+def get_coordinate_of_click(event) -> QPoint or None:
+    """Returns a QPoint with coordinates of mouse click event or None if the click event
+    was not inside the graph.
+    """
+    if event.inaxes and event.name == "button_press_event":
+        return QPoint(int(event.xdata), int(event.ydata))
     else:
-
-        self.w = SecondaryWindow(self.main_window)
-        self.w.add_canvas(GraphCanvas())
-        self.w.show()
-
-        self.last_change = None
-        self.X = None
-        self.Y = None
-
-        print("unten")
-        print("x: %s, y: %s" % (event.xdata, event.ydata))
-        # coordinates need to be adjusted when imshow is used
-        # instead of pcolormesh for 2D plots
-        x = int(event.xdata)  # + 0.5)
-        y = int(event.ydata)  # + 0.5)
-
-        self.w.canvas.figure.clear()
-        self.w.canvas.axes = self.w.canvas.figure.add_subplot(111)
-        self.w.canvas.axes.plot(self.current_selection.x_data[y, :], self.current_selection.z_data[y, :])
-
-        if not self.last_change:
-            self.X = x
-            self.Y = y
-            self.last_change = self.current_selection.z_data[y, x]
-            # self.currentView.z_data[y, x] = 0.0
-            self.figure_canvas_2d.draw_graph(self.current_selection)
-        else:
-            print("Last: %s" % self.last_change)
-            self.current_selection.z_data[self.Y, self.X] = self.last_change
-            # self.last_change = self.currentView.z_data[y, x]
-            self.current_selection.z_data[y, x] = 0.0
-            self.X = x
-            self.Y = y
-            self.figure_canvas_2d.draw_graph(self.current_selection)
-            print(self.X, self.Y)
-        print(self.current_selection.z_data[x, y])
+        return None
 
 
-
-
-def origin_point(canvas, event):
-    if event.name == "button_press_event":
-        self.P1 = QPoint(int(event.xdata), int(event.ydata))
-        self.cid_release = self.figure_canvas_2d.figure.canvas.mpl_connect('button_release_event', self.rectangle_test)
-        self.cid_move = self.figure_canvas_2d.figure.canvas.mpl_connect('motion_notify_event', self.rectangle_test)
-
-
-def mouse_over_value(event):
-    print("z: %s µm " % self.get_data_from_point(QPoint(int(event.xdata), int(event.ydata))))
-    print("Coords: X %s | Y %s " % (event.xdata, event.ydata))
-    time.sleep(0.2)
-
-
-def about():
-
-    if not self.cid:
-        if len(self.figure_canvas_2d.figure.get_axes()) > 1:
-            self.cid_press = self.figure_canvas_2d.figure.canvas.mpl_connect('button_press_event', self.origin_point)
-            self.cid = self.figure_canvas_2d.figure.canvas.mpl_connect('button_press_event', self.click_on_raster_image)
-            self.cid = self.figure_canvas_2d.figure.canvas.mpl_connect("motion_notify_event", self.mouse_over_value)
-    else:
-        figure_canvas.figure.canvas.mpl_disconnect(self.cid)
-        cid = None
-
-def get_data_from_point(self, point: QPoint):
-    """"""
-    return self.current_selection.z_data[point.y(), point.x()]
+def points_are_not_equal(point1: QPoint, point2: QPoint) -> bool:
+    """Checks if two points have the same coordinates."""
+    return point1.x() != point2.x() and point1.y() != point2.y()
